f69f4c74404073c4a91089d101922eeba5c10582
Austin Towler <44880258+altowler@users.noreply.github.com>
2021-09-14 11:17:52 -0400

    adds completed assignment

---
+++ SweetnSaltyConsole/SweetnSalty/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SweetnSalty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sweet = "Sweet";
+            string salty = "Salty";
+            string both = "Sweet'n Salty";
+
+            int sweetTotal = 0;
+            int saltyTotal = 0;
+            int bothTotal = 0;
+
+            for(int i = 1; i <= 1000; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    System.Console.Write(both + " ");
+                    bothTotal++;
+                }
+                else if (i % 3 == 0)
+                {
+                    System.Console.Write(sweet + " ");
+                    sweetTotal++;
+                }
+                else if (i % 5 == 0)
+                {
+                    System.Console.Write(salty + " ");
+                    saltyTotal++;
+                }
+                else { System.Console.Write(i + " "); }
+
+                if(i % 10 == 0)
+                {
+                    System.Console.WriteLine();
+                }
+            }
+
+            System.Console.WriteLine($"Sweets: {sweetTotal}");
+            System.Console.WriteLine($"Salty: {saltyTotal}");
+            System.Console.WriteLine($"Sweet'n Salty: {bothTotal}");
+
+        }
+    }
+}
